73823661e5148c684105f680d2a2f913ae2b5b5b
lancewburklin <1677@holbertonschool.com>
2020-06-11 18:29:53 +0000

    Sorts but with bubbles

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array using Bubble Sort
+ * @array: The array to sort
+ * @size: Size of the array
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	int flag = 0;
+	int temp;
+
+	if (size < 2)
+		return;
+	while (array && i < size - 1)
+	{
+		flag = 0;
+		if (array[i] > array[i + 1])
+		{
+			temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+			print_array(array, size);
+			flag = 1;
+		}
+		if (flag == 0)
+			i++;
+		else
+			i = 0;
+	}
+}
