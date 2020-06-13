f39a2a7dab1f2303bcaf426bb5b79bc7937e3517
lancewburklin <1677@holbertonschool.com>
2020-06-13 04:08:42 +0000

    Fixed double declaration

--- print_list.c
+++ print_list.c
@@ -20,26 +20,4 @@ void print_list(const listint_t *list)
 		list = list->next;
 	}
 	printf("\n");
-}#include <stdio.h>
-#include "sort.h"
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-	int i;
-
-	i = 0;
-	while (list)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", list->n);
-		++i;
-		list = list->next;
-	}
-	printf("\n");
 }
